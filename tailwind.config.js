e857a1f852fdcb94dcc3e7236a786c376e6a7c62
Sujay Chowdhury <98274125+NoobSujay@users.noreply.github.com>
2024-10-29 12:15:53 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        robota: ["Roboto", "sans-serif"],
+        bebas: ["Bebas Neue", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
